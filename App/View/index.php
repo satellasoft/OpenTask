a6f396dc3a9cc2336b7f90ef7adf0a695cf9a717
SatellaSoft <satellasoft@gmail.com>
2019-10-24 22:12:19 -0300

    Layout responsive menu

--- App/View/index.php
+++ App/View/index.php
@@ -10,65 +10,21 @@
   <?php $this->RunHeader(); ?>
 </head>
 <body>
-  <div class="p-2 mb-2 bg-primary text-white">
+  <div class="p-2 mb-2 bg-primary text-white d-none d-sm-block">
     Open Task
   </div>
+  <?php
+  require_once("partials/menu-mobile.php");
+   ?>
   <div class="row">
-    <div class="col-md-2">
+    <div class="col-md-2 d-none d-sm-block">
       <?php
       if($GLOBALS["logged"]){
-        ?>
-        <div class="p-1">
-          <!--MAIN-->
-          <div class="card border-info">
-            <div class="card-header">
-              Minhas tarefas
-            </div>
-            <div class="card-body">
-              <ul class="menu-side">
-                <li><a href="<?=BASE?>">Início</a></li>
-                <li><a href="#">Meus projetos</a></li>
-                <li><a href="#">Calendário</a></li>
-              </ul>
-            </div>
-          </div>
-
-          <!--ADMINISTRATOR-->
-          <?php
-          if($_SESSION["p"] == 1){
-           ?>
-          <div class="card border-info mt-3">
-            <div class="card-header">
-              Administrator
-            </div>
-            <div class="card-body">
-              <ul class="menu-side">
-                <li><a href="<?=BASE?>user/">Usuário</a></li>
-                <li><a href="<?=BASE?>project">Projeto</a></li>
-              </ul>
-            </div>
-          </div>
-        <?php } ?>
-
-
-        <!--ADMINISTRATOR-->
-        <div class="card border-info mt-3">
-          <div class="card-header">
-            Configurações
-          </div>
-          <div class="card-body">
-            <ul class="menu-side">
-              <li><a href="<?=BASE?>user/passwordchange">Alterar senha</a></li>
-              <li><a href="<?=BASE?>login/logout">Sair</a></li>
-            </ul>
-          </div>
-        </div>
-        </div>
-        <?php
+        require_once("partials/menu-desktop.php");
       }
       ?>
     </div>
-    <div class="col-md-10">
+    <div class="col-md-10 col-xs-12">
       <?php $this->RunContent(); ?>
     </div>
   </div>
---
+++ App/View/partials/menu-desktop.php
@@ -0,0 +1,46 @@
+<div class="p-1">
+  <!--MAIN-->
+  <div class="card border-info">
+    <div class="card-header">
+      Minhas tarefas
+    </div>
+    <div class="card-body">
+      <ul class="menu-side">
+        <li><a href="<?=BASE?>">Início</a></li>
+        <li><a href="#">Meus projetos</a></li>
+        <li><a href="#">Calendário</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <!--ADMINISTRATOR-->
+  <?php
+  if($_SESSION["p"] == 1){
+   ?>
+  <div class="card border-info mt-3">
+    <div class="card-header">
+      Administrator
+    </div>
+    <div class="card-body">
+      <ul class="menu-side">
+        <li><a href="<?=BASE?>user/">Usuário</a></li>
+        <li><a href="<?=BASE?>project">Projeto</a></li>
+      </ul>
+    </div>
+  </div>
+<?php } ?>
+
+
+<!--ADMINISTRATOR-->
+<div class="card border-info mt-3">
+  <div class="card-header">
+    Configurações
+  </div>
+  <div class="card-body">
+    <ul class="menu-side">
+      <li><a href="<?=BASE?>user/passwordchange">Alterar senha</a></li>
+      <li><a href="<?=BASE?>login/logout">Sair</a></li>
+    </ul>
+  </div>
+</div>
+</div>
