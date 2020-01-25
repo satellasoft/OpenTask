520ee53e4e7ca4c4d7989aeab2dc14cc69140eab
SatellaSoft <satellasoft@gmail.com>
2020-01-25 11:52:09 -0300

    Implementando <noscript> por segurança

--- App/View/index.php
+++ App/View/index.php
@@ -34,5 +34,9 @@
   <script src="<?=BASE?>js/jquery-3.4.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  <noscript>
+  <meta http-equiv="refresh" content="0;url=https://stackoverflow.com/questions/18810369/noscript-to-redirect-i-need-to-redirect">
+  </noscript>
 </body>
 </html>
